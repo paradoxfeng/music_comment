3bb2fad6236dfcc39b3560b2ed047040ffdaf586
paradoxfeng <695776300@qq.com>
2018-04-24 04:00:24 +0800

    网易云音乐评论

---
+++ comment.py
@@ -0,0 +1,88 @@
+import requests
+from config import *
+from lxml import etree
+import json
+
+class Comment:
+    def __init__(self):
+        self.headers = HEADERS
+
+
+    def start_urls(self):
+        start_urls = START_URLS
+        for url in start_urls:
+            for x in range(65,91):
+                response = requests.get(url.format(initial=x),headers=self.headers)
+                yield response
+
+
+    def parse_start_urls(self,responses): #解析start_urls获得artist_id,并生成歌手专辑的url的response
+        for response in responses:
+            html = etree.HTML(response.text)
+            artists = html.xpath('//ul[@id="m-artist-box"]/li')
+            for artist in artists:
+                artist_name = artist.xpath('.//a[@class="nm nm-icn f-thide s-fc0"]/text()')[0]
+                artist_id = artist.xpath('.//a[@class="nm nm-icn f-thide s-fc0"]/@href')[0][11:].replace('=','')
+                params = {'id':artist_id,'limit':2000}
+                response = requests.get('http://music.163.com/artist/album?',params=params,headers=self.headers)
+                yield response
+
+
+
+    def parse_album_urls(self,responses): #解析album_urls获得album_id,并生成专辑内歌曲页面的response
+        for response in responses:
+            html = etree.HTML(response.text)
+            albums_href = html.xpath('//*[@id="m-song-module"]/li//a[@class="msk"]/@href')
+            for album_href in albums_href:
+                album_id = album_href.replace('/album?id=','')
+                params = {'id':album_id}
+                response = requests.get('http://music.163.com/album?',params=params,headers=self.headers)
+                yield response
+
+
+    def parse_song_urls(self,responses): #解析song_urls获得song_id,并生成非加密的歌曲评论（前20条评论）的response（格式为json）
+        for response in responses:
+            html = etree.HTML(response.text)
+            songs = html.xpath('//ul[@class="f-hide"]/li')
+            for song in songs:
+                song_id = song.xpath('./a/@href')[0].replace('/song?id=','')
+                response = requests.get('http://music.163.com/api/v1/resource/comments/R_SO_4_{song_id}'.format(song_id=song_id),headers=self.headers)
+                yield response
+
+
+
+    def get_comment_urls(self,responses): #解析json并生成所以评论的urls
+        for response in responses:
+            song_id = response.url.replace('http://music.163.com/api/v1/resource/comments/R_SO_4_','')
+            text = response.text
+            result = json.loads(text)
+            total = result['total']
+            integer = int(total) // 100
+            for x in range(integer + 1):
+                response = requests.get('http://music.163.com/api/v1/resource/comments/R_SO_4_{song_id}?limit=100&offset={offset}'.format(song_id=song_id,offset=x*100),headers=self.headers)
+                yield response
+
+
+    def get_comment(self,responses):
+        for response in responses:
+            text = response.text
+            result = json.loads(text)
+            comments = result['comments']
+            for comment in comments:
+                comment = comment['content']
+                print(comment)
+
+
+
+
+    def start(self):
+        response = self.start_urls()
+        response = self.parse_start_urls(response)
+        response = self.parse_album_urls(response)
+        response = self.parse_song_urls(response)
+        response = self.get_comment_urls(response)
+        self.get_comment(response)
+
+
+c = Comment()
+c.start()
---
+++ config.py
@@ -0,0 +1,32 @@
+HEADERS = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, sdch',
+    'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+    'DNT': '1',
+    'Host': 'music.163.com',
+    'Pragma': 'no-cache',
+    'Referer': 'http://music.163.com/',
+    'Upgrade-Insecure-Requests': '1',
+    'Cookie':'_iuqxldmzr_=32; _ntes_nnid=5151c228a2fbc77b980eb8aadaf39c26,1517969449404; _ntes_nuid=5151c228a2fbc77b980eb8aadaf39c26; __utmz=94650624.1517969457.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); UM_distinctid=1617ba1b9ae832-09c097e0ca6585-5d4e211f-1fa400-1617ba1b9af5da; vjuids=6fb93d9a3.1617ba1c136.0.ae6a3756152c; P_INFO=tiancaiyujinfeng7@163.com|1523170884|2|cc|00&99|null&null&null#zhj&330100#10#0#0|&0||tiancaiyujinfeng7@163.com; __f_=1523524231688; WM_TID=eufXFQv3J98gMXqHemVNG5g7Go3WQrzi; Province=0571; City=0571; vjlast=1518197654.1524202398.21; NNSSPID=4f429750c4bb46ca89fbe8d4e856a55e; vinfo_n_f_l_n3=6ed56a530e5d9bc1.1.4.1518197653875.1521393748910.1524202460372; __utmc=94650624; __utma=94650624.1151711844.1517969457.1524325288.1524330869.14; JSESSIONID-WYYY=Wmlu3V2u2R3tQWxpB5XkNDHzX3HIPdnizK0kXCg19W0V6qojRI82k0v%2B7JF7IhWPJQui7PduaZ6I3agO%2BxmxAkQsivs%5CIpkw7G9JehB%2F%2BYpf4rUXuY%2FaYr%2FVvqhCt1mfgM2ZF%2F%2FditRRDScjlRnPma9aQVOhaDKyJmkPVRNSEYprujh1%3A1524333941838; __remember_me=true; MUSIC_U=d91dee0665a160d44e8793ea37f8926c8ae7d768b71739cd72a6906814873e01892da7aeced779ec098c584a70116b164922ed9102ac8cbccddbaee124c51eaf32db1626929b2c19; __csrf=4af4268292ca8d5656ad975793385745; __utmb=94650624.17.10.1524330869',
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
+}
+
+
+START_URLS = ['http://music.163.com/discover/artist/cat?id=1001&initial={initial}',
+                      # 'http://music.163.com/discover/artist/cat?id=1002&initial={initial}',
+                      # 'http://music.163.com/discover/artist/cat?id=1003&initial={initial}',
+                      # 'http://music.163.com/discover/artist/cat?id=2001&initial={initial}',
+                      # 'http://music.163.com/discover/artist/cat?id=2002&initial={initial}',
+                      # 'http://music.163.com/discover/artist/cat?id=2003&initial={initial}',
+                      # 'http://music.163.com/discover/artist/cat?id=6001&initial={initial}',
+                      # 'http://music.163.com/discover/artist/cat?id=6002&initial={initial}',
+                      # 'http://music.163.com/discover/artist/cat?id=6003&initial={initial}',
+                      # 'http://music.163.com/discover/artist/cat?id=7001&initial={initial}',
+                      # 'http://music.163.com/discover/artist/cat?id=7002&initial={initial}',
+                      # 'http://music.163.com/discover/artist/cat?id=7003&initial={initial}',
+                      # 'http://music.163.com/discover/artist/cat?id=4001&initial={initial}',
+                      # 'http://music.163.com/discover/artist/cat?id=4002&initial={initial}',
+                      # 'http://music.163.com/discover/artist/cat?id=4003&initial={initial}'
+                      ]
